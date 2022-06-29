247e58563647dfbecdade2bf5803885252de0bcd
hilderney <hilderney@gmail.com>
2022-06-28 22:39:09 -0300

    Flexbox, sizes and overflows

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
+import logo from '../../logo.svg'
 import './Style/Header.css'
 
 export function AppHeader() {
     return (
         <header> 
-            <h1> Header </h1>
+            <div className="logo">
+            <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <div className="title">
+                Gerador de CNPJ
+            </div>
+            <div className="navbar-wrapper">
+                <a href="http://" target="_blank" rel="noopener noreferrer"> Link</a>
+            </div>
         </header>
     )
 }
