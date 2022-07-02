88b6afaded681c000a0ecec20d8976fae1dbd128
hilderney <hilderney@gmail.com>
2022-07-01 22:12:20 -0300

    iniciando tela gerador

--- src/pages/generator/Generator.tsx
+++ src/pages/generator/Generator.tsx
@@ -3,10 +3,25 @@ import './style/Generator.css'
 
 const Generator: React.FC = () => {
 
+  function generateDocument(): any {
+    console.log('hi')
+  }
+
   return (
-    <p>
-      Generator Page
-    </p>
+    <div className='generator'>
+      <div className="panel">
+        <div className="col inputs">
+          <input className='inputCnpj' type="text" />
+          <button onClick={generateDocument()}> GerarCNPJ</button>
+        </div>
+        <div className="col info">
+          <p>
+            Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
+          </p>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
