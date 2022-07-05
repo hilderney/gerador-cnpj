f6511fafb8fdb95012ed3d18c64ac441a328148d
hilderney <hilderney@gmail.com>
2022-07-05 13:08:27 -0300

    dealing with data handlers for CNPJ

---
+++ src/domain/cnpj/GerarCnpj.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const GerarCnpj: any = () => {
+
+  let resultado: string;
+  let n = 9;
+  let n1 = gera_random(n);
+  let n2 = gera_random(n);
+  let n3 = gera_random(n);
+  let n4 = gera_random(n);
+  let n5 = gera_random(n);
+  let n6 = gera_random(n);
+  let n7 = gera_random(n);
+  let n8 = gera_random(n);
+  let n9 = 0;//gera_random(n);
+  let n10 = 0;//gera_random(n);
+  let n11 = 0;//gera_random(n);
+  let n12 = 1;//gera_random(n);
+
+  let d1 = n12 * 2 + n11 * 3 + n10 * 4 + n9 * 5 + n8 * 6 + n7 * 7 + n6 * 8 + n5 * 9 + n4 * 2 + n3 * 3 + n2 * 4 + n1 * 5;
+  d1 = 11 - (mod(d1, 11));
+  if (d1 >= 10)
+    d1 = 0;
+
+  let d2 = d1 * 2 + n12 * 3 + n11 * 4 + n10 * 5 + n9 * 6 + n8 * 7 + n7 * 8 + n6 * 9 + n5 * 2 + n4 * 3 + n3 * 4 + n2 * 5 + n1 * 6;
+  d2 = 11 - (mod(d2, 11));
+  if (d2 >= 10)
+    d2 = 0;
+
+  resultado = '' + n1 + n2 + '.' + n3 + n4 + n5 + '.' + n6 + n7 + n8 + '/' + n9 + n10 + n11 + n12 + '-' + d1 + d2;
+  console.log('CNPJ gerado: ', resultado);
+
+  return resultado;
+}
+
+export default GerarCnpj
+
+function gera_random(n: number): number {
+  var ranNum = Math.round(Math.random() * n);
+  return ranNum;
+}
+
+function mod(dividendo: number, divisor: number): number {
+  return Math.round(dividendo - (Math.floor(dividendo / divisor) * divisor));
+}
--- src/pages/generator/Generator.tsx
+++ src/pages/generator/Generator.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
+import GerarCnpj from '../../domain/cnpj/GerarCnpj';
+import ValidarCnpj from '../../domain/cnpj/ValidarCnpj';
 import './style/Generator.css'
 
 const Generator: React.FC = () => {
 
+  // Gerar Novo CNPJ
+  // Validar CNPJ no Campo de Texto
+  // Gerar CNPJs Filiais
+  // Validar CNPJs de Filiais
+
+
   function generateDocument(): any {
-    console.log('hi')
+    const cnpjGerado = GerarCnpj();
+    console.log('Gerando CNPJ...', cnpjGerado);
+    console.log('Validar CNPJ...', ValidarCnpj(cnpjGerado.toString()));
+
   }
+
+
 
   return (
     <div className='generator'>
       <div className="panel">
         <div className="col inputs">
           <input className='inputCnpj' type="text" />
-          <button onClick={generateDocument()}> GerarCNPJ</button>
+          <button onClick={() => generateDocument()}> GerarCNPJ</button>
         </div>
         <div className="col info">
           <p>
